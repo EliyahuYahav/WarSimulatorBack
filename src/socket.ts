a3e9e09709a57f9c25d997f8336f555a7d525b2a
Eliyaou52 <e9189891@gmail.com>
2024-11-14 17:44:17 +0200

    fix the socket lest upload

--- src/socket.ts
+++ src/socket.ts
@@ -1,8 +1,16 @@
 import { Server } from "socket.io";
-import { Server as HTTPServer } from 'http';
-import Organizations from "./modules/organizationMoudle";
+import { Server as HTTPServer } from "http";
+import Organizations, { IOrganizations, IResources } from "./modules/organizationMoudle";
+import { IMissile } from "./modules/missileMoudle";
+import Missile from "./modules/missileMoudle"
 
 let socketOn: boolean = false;
+
+const RetuneAmount = async (organization: string, missile:string)=>{
+  const newOrganizations: IOrganizations | null= await Organizations.findOne({name: organization})
+  const correctMissile:IResources | undefined = newOrganizations?.resources.find((miss)=>{ return miss.name == missile})
+  return correctMissile?.amount
+}
 
 export const StartSocket = (httpServer: HTTPServer) => {
   if (!socketOn) {
@@ -15,18 +23,15 @@ export const StartSocket = (httpServer: HTTPServer) => {
     socketOn = true;
     io.on("connection", (socket) => {
       socket.on("StartAttack", async ({ missile, organization }) => {
-        await Organizations.findOneAndUpdate(
-          { 
-            name: organization, 
-            "resources.name": missile, 
-            "resources.amount": { $gt: 0 }
-          },
-          { 
-            $inc: { "resources.$.amount": -1 }
-          }
-        );        
+      await Organizations.updateOne({ name: organization, "resources.name": missile, "resources.amount": { $gt: 0 }, },{
+            $inc: { "resources.$.amount": -1 },trim: true, });
+      await Missile.updateOne({name: missile, $set: {"status": "launched"}})
+        socket.emit(
+          "amount-left", await RetuneAmount(organization, missile)
+        );
       });
-      socket.broadcast.emit('StartAttack', );
     });
   }
 };
+
+
